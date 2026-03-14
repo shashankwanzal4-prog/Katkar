ee63b05b364e6845a0072fa9ee3776f819a4be61
Shashank Wanzal <shashankwanzal4@gmail.com>
2026-03-14 19:35:49 +0530

    Mongo

--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
-const express=require("express")
-const mongoose=require("mongoose")
-const cors=require("cors")
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
 
-const app=express()
+const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect("mongodb://127.0.0.1:27017/katkar")
+// MongoDB Atlas connection
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err))
 
 app.get("/",(req,res)=>{
 res.send("Katkar Events API Running")
 })
 
-app.listen(5000,()=>{
-console.log("Server running on port 5000")
+app.get("/events",(req,res)=>{
+res.json([
+{ name:"Wedding Planning", location:"Pune"},
+{ name:"Corporate Event", location:"Mumbai"},
+{ name:"Birthday Party", location:"Nagpur"}
+])
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+console.log("Server running on port "+PORT)
 })
